ee01b52a166130924ef9af0fed439389192f4c5e
Issayev Adilet <issayev.adilet@gmail.com>
2016-06-19 19:08:23 +0600

    new files

--- app/start.php
+++ app/start.php
@@ -7,9 +7,10 @@ session_start();
 ini_set('display_errors', 'On');
 
 define('APP_ROOT', __DIR__);
-define('BASE_URL', 'http://aikune');
+define('BASE_URL', 'http://aikune.kz');
 define('VIEW_ROOT', APP_ROOT . '/views');
 
+// $db = new PDO('mysql:host=srv-db-plesk06.ps.kz:3306;dbname=aikunekz_data', 'aikun_user', 'k&k3Sw22');
 $db = new PDO('mysql:host=localhost;dbname=aikune', 'bigstore_user', 'secret');
 
 require 'functions.php';
